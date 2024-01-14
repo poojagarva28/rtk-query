ca47551ff3c64befe984b12a7af4b8b6ce811bdc
pooja <poojagarva123@gmail.com>
2024-01-14 11:12:13 +0530

    add, update and delete post

--- src/components/posts.js
+++ src/components/posts.js
@@ -1,23 +1,71 @@
-import { usePostsQuery, usePostQuery } from "../services/postApi";
+import {
+  usePostsQuery,
+  usePostQuery,
+  useAddPostMutation,
+  useUpdatePostMutation,
+  useDeletePostMutation,
+} from "../services/postApi";
 
 const Posts = () => {
   const { data: posts } = usePostsQuery();
   const { data: singlePost } = usePostQuery(2);
 
   return (
-    <ul>
-      {posts?.map((post) => (
-        <li key={post.id}>
-          {post.title}
-          <br />
-          <br />
-          {JSON.stringify(singlePost)}
-          <br />
-          <br />
-        </li>
-      ))}
-    </ul>
+    <>
+      <AddingPost />
+      <UpdatingPost />
+      <DeletePost />
+      <ul>
+        {posts?.map((post) => (
+          <li key={post.id}>
+            {post.title}
+            <br />
+            <br />
+            {JSON.stringify(singlePost)}
+            <br />
+            <br />
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
 export default Posts;
+
+const AddingPost = () => {
+  const [addPost] = useAddPostMutation();
+  // const { refetch } = usePostsQuery();
+  const newPost = {
+    title: "new post",
+    userId: 1234,
+    id: 1234,
+    body: "new body",
+  };
+  return (
+    <button
+      onClick={() => {
+        addPost(newPost);
+        // refetch();
+      }}
+    >
+      Add Post
+    </button>
+  );
+};
+
+const UpdatingPost = () => {
+  const [updatePost] = useUpdatePostMutation();
+  const newPost = {
+    title: "new post2",
+    userId: 1234,
+    id: 1234,
+    body: "new body",
+  };
+  return <button onClick={() => updatePost(1234, newPost)}>Update Post</button>;
+};
+
+const DeletePost = () => {
+  const [deletePost] = useDeletePostMutation();
+  return <button onClick={() => deletePost(2)}>Delete Post</button>;
+};
--- src/services/postApi.js
+++ src/services/postApi.js
@@ -2,17 +2,49 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const postApi = createApi({
   reducerPath: "postApi",
+  tagTypes: ["Post"],
   baseQuery: fetchBaseQuery({
     baseUrl: "https://jsonplaceholder.typicode.com/",
   }),
   endpoints: (builder) => ({
     posts: builder.query({
       query: () => `/posts`,
+      providesTags: ["Post"],
     }),
     post: builder.query({
       query: (id) => `/posts/${id}`,
+      providesTags: ["Post"],
+    }),
+    addPost: builder.mutation({
+      query: (post) => ({
+        url: "/posts",
+        method: "POST",
+        body: post,
+      }),
+      invalidatesTags: ["Post"],
+    }),
+    updatePost: builder.mutation({
+      query: (id, ...rest) => ({
+        url: `/posts/${id}`,
+        method: "PUT",
+        body: rest,
+      }),
+      invalidatesTags: ["Post"],
+    }),
+    deletePost: builder.mutation({
+      query: (id) => ({
+        url: `/posts/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Post"],
     }),
   }),
 });
 
-export const { usePostsQuery, usePostQuery } = postApi;
+export const {
+  usePostsQuery,
+  usePostQuery,
+  useAddPostMutation,
+  useUpdatePostMutation,
+  useDeletePostMutation,
+} = postApi;
